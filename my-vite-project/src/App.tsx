df1d36df875b056325c07f5cbc3c205a0d4263fd
BekjonUz <mightyking584@gmail.com>
2025-10-17 18:27:24 +0500

    add

--- my-vite-project/src/App.tsx
+++ my-vite-project/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className=' text-amber-800'>App</div>
-  )
-}
+    <div className="w-[1200px] mx-auto">
+      <div className="bg-red-600 w-[100px] h-[100px] flex justify-center items-center text-red-600 rounded-[50%]">Hello</div>
+      <div className="bg-yellow-400 w-[100px] h-[100px] flex justify-center items-center text-yellow-400 rounded-[50%]">Hello</div>
+      <div className="bg-green-600 w-[100px] h-[100px] flex justify-center items-center text-green-600 rounded-[50%]">Hello</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
