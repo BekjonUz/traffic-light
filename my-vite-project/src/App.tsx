2cfdb00aa4436c66ab624884f067e146046341ad
BekjonUz <mightyking584@gmail.com>
2025-10-17 18:46:48 +0500

    add

--- my-vite-project/src/App.tsx
+++ my-vite-project/src/App.tsx
@@ -16,12 +16,18 @@ useEffect(() => {
   }, 1000);
   return () => clearInterval(interval);
 }, []);
-
+  
   return (
     <div className="w-[1200px] mx-auto">
-      <div className="bg-red-600 w-[100px] h-[100px] flex justify-center items-center text-red-600 rounded-[50%]">Hello</div>
-      <div className="bg-yellow-400 w-[100px] h-[100px] flex justify-center items-center text-yellow-400 rounded-[50%]">Hello</div>
-      <div className="bg-green-600 w-[100px] h-[100px] flex justify-center items-center text-green-600 rounded-[50%]">Hello</div>
+      {
+        count%2===0?<div className="bg-red-600 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]">{time}</div>:<div className="bg-red-300 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]">Wait</div>
+      }
+      {
+        time>3?<div className="bg-yellow-200 w-[100px] h-[100px] flex justify-center items-center rounded-[50%]"></div>:<div className="bg-yellow-400 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]">{time}</div>
+      }
+      {
+        count%2!==0?<div className="bg-green-600 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]">{time}</div>:<div className="bg-green-300 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]">Wait</div>
+      }
       <h1>{time}</h1>
       <p>{count}</p>
     </div>
