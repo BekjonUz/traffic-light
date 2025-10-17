3621874695f87c6f7b1572961142e7a42ef2140e
BekjonUz <mightyking584@gmail.com>
2025-10-17 19:11:39 +0500

    add

--- my-vite-project/src/App.tsx
+++ my-vite-project/src/App.tsx
@@ -23,13 +23,13 @@ useEffect(() => {
         <div className="flex items-center">
         <div className="flex flex-col bg-[#000] p-[1rem] gap-[0.3rem]">
         {
-        count%2===0 && time>3 ?<div className="bg-red-700 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>:<div className="bg-red-500 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>
+        count%2===0 && time>3 ?<div className="bg-red-500 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>:<div className="bg-red-700 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>
       }
       {
-        time>3?<div className="bg-yellow-300 w-[100px] h-[100px] flex justify-center items-center rounded-[50%]"></div>:<div className="bg-yellow-500 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>
+        time>3?<div className="bg-yellow-500 w-[100px] h-[100px] flex justify-center items-center rounded-[50%]"></div>:<div className="bg-yellow-300 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>
       }
       {
-        count%2!==0?<div className="bg-green-600 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>:<div className="bg-green-500 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>
+        count%2!==0 && time>3 ?<div className="bg-green-400 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>:<div className="bg-green-700 w-[100px] h-[100px] flex justify-center items-center text-white rounded-[50%]"></div>
       }
       </div>
       <div className="w-[10rem] h-[5rem] bg-[#00000085]">
